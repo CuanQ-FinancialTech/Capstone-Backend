4243d4a686389ac37340fc4f777d7ad9b0738337
rizqiNurrodji <rizqinurrodji@gmail.com>
2025-06-13 01:46:03 +0700

    CORS enable all

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const port = configService.get<number>('PORT') || 3000;
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
     credentials: true,
   });
 
